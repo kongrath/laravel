b75a8b67c89b9584167dd3dfa3c4dfda065036e5
joe <kongrath.jojo@gmail.com>
2015-01-25 14:32:46 +0700

    start to implement on dev branch

---
+++ app/views/includes/head.blade.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+
+<title>Laravel Cown Team Dev.</title>
+
+<!-- load bootstrap from a cdn -->
+<!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="assets/css/freelancer.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    @include('includes.head')
+</head>
+<body id="page-top" class="index">
+
+    @include('includes.header')
+
+    @yield('content')
+
+    @include('includes.footer')
+
+    @include('includes.script')
+
+</body>
+</html>
